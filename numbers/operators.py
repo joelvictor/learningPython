d3527f68bf7a8f79d15feca856fa05c73baa3950
Joel Victor <joelvaluiz@gmail.com>
2025-01-11 19:58:10 -0300

    Number Python

---
+++ numbers/operators.py
@@ -0,0 +1,33 @@
+x = 1+1
+
+print ("sum ", x)
+
+y = 2.5
+
+#result is a float
+print (x*y) 
+
+k = (10/2) * 5
+
+print(k)
+# can not have spaces between the operators
+
+exp = 2**3
+
+print(exp)
+
+# integer division
+intdivision = 10//3
+print("intdivision ", intdivision)
+
+# modulo
+modulo = 10 % 3
+print("modulo ",modulo)
+
+'''
+Multi line comment
+x
+'''
+
+#operator assign
+x+=3
